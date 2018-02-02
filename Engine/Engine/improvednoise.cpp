d6410f52bb4c31eb82e64aae50bf51d575323eaa
UAD\1501082 <1501082@abertay.ac.uk>
2018-02-02 14:24:03 +0000

    Added Engine. Includes White Noise and Smoothing.

---
+++ Engine/Engine/improvednoise.cpp
@@ -0,0 +1,15 @@
+#include "improvednoise.h"
+
+
+
+improvednoise::improvednoise()
+{
+}
+improvednoise::improvednoise(const improvednoise &other)
+{
+	
+}
+
+improvednoise::~improvednoise()
+{
+}
---
+++ Engine/Engine/improvednoise.h
@@ -0,0 +1,22 @@
+// Improved Perlin Noise
+// Developmed by Ken Perlin [http://mrl.nyu.edu/~perlin/noise/]
+// Translated into C++ by James Clayton
+
+#pragma once
+class improvednoise
+{
+public:
+	improvednoise();
+	improvednoise(const improvednoise&);
+	~improvednoise();
+
+	void Initialise();
+	void Shutdown();
+	void Sample();
+
+private:
+
+
+
+};
+
