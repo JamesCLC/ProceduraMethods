bcb3a1984002c222832f4e039f13d77819580acb
JamesCLC <j.clayton.3096@gmail.com>
2018-04-30 15:03:46 +0100

    - Forgot to inlcude the fixed cylinder model txt file in the last commit. - Added a picture of the cactus looking more cactus shaped. - Tidied up the skydome class.

--- Engine/Engine/skydomeclass.h
+++ Engine/Engine/skydomeclass.h
@@ -26,9 +26,9 @@ private:
 		float tu, tv;
 		float nx, ny, nz;
 	};
-	//The VertexType for the sky dome only requires position.We generate the colors based on the height of the sky dome.
 
-		struct VertexType
+	//The VertexType for the sky dome only requires position. We generate the colors based on the height of the sky dome.
+	struct VertexType
 	{
 		D3DXVECTOR3 position;
 	};
@@ -65,67 +65,3 @@ private:
 
 #endif
 
-//////////////////////////////////////////////////////////////////////////////////
-//// Filename: skydomeclass.h
-//////////////////////////////////////////////////////////////////////////////////
-//#ifndef _SKYDOMECLASS_H_
-//#define _SKYDOMECLASS_H_
-//
-//
-////////////////
-//// INCLUDES //
-////////////////
-//#include <d3d11.h>
-//#include <directxmath.h>
-//#include <fstream>
-//using namespace DirectX;
-//using namespace std;
-//
-//
-//////////////////////////////////////////////////////////////////////////////////
-//// Class name: SkyDomeClass
-//////////////////////////////////////////////////////////////////////////////////
-//class SkyDomeClass
-//{
-//private:
-//	struct ModelType
-//	{
-//		float x, y, z;
-//		float tu, tv;
-//		float nx, ny, nz;
-//	};
-//
-//	struct VertexType
-//	{
-//		XMFLOAT3 position;
-//	};
-//
-//public:
-//	SkyDomeClass();
-//	SkyDomeClass(const SkyDomeClass&);
-//	~SkyDomeClass();
-//
-//	bool Initialize(ID3D11Device*);
-//	void Shutdown();
-//	void Render(ID3D11DeviceContext*);
-//
-//	int GetIndexCount();
-//	XMFLOAT4 GetApexColor();
-//	XMFLOAT4 GetCenterColor();
-//
-//private:
-//	bool LoadSkyDomeModel(char*);
-//	void ReleaseSkyDomeModel();
-//
-//	bool InitializeBuffers(ID3D11Device*);
-//	void ReleaseBuffers();
-//	void RenderBuffers(ID3D11DeviceContext*);
-//
-//private:
-//	ModelType* m_model;
-//	int m_vertexCount, m_indexCount;
-//	ID3D11Buffer *m_vertexBuffer, *m_indexBuffer;
-//	XMFLOAT4 m_apexColor, m_centerColor;
-//};
-//
-//#endif
